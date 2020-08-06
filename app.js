f96f286b76f80e554b7f4118e776c3de0a981fd9
icedike <icedike@gmail.com>
2020-08-06 21:43:10 +0800

    feat: add routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 
 require('./config/mongoose')
 
@@ -10,6 +11,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(routes)
 app.get('/', (req, res) => {
   res.render('index')
 })
