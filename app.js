7cdc8aa1523fc1f0a486bac79b5161c860fdca7e
icedike <icedike@gmail.com>
2020-08-08 22:41:47 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'hbs')
