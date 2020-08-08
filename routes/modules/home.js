ddaead3fd156219cce925eb7dccb73ecd7507c7e
icedike <icedike@gmail.com>
2020-08-08 17:09:05 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const { Category, Record } = require('../../models/record')
+const record = require('../../models/record')
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -20,6 +21,33 @@ router.get('/', (req, res) => {
             record.date = record.date.replace(/-/g, '/')
             slashRecords.push(record)
           })
+          return res.render('index', { categories, records: slashRecords, totalAmount })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sort', (req, res) => {
+  const categoryId = req.query.sort_id
+  Category.find()
+    .lean()
+    .then(categories => {
+      return Record.find({ category: categoryId })
+        .populate('category')
+        .lean()
+        .then(records => {
+          const slashRecords = []
+          records.forEach(function (record) {
+            record.date = record.date.replace(/-/g, '/')
+            slashRecords.push(record)
+          })
+          let totalAmount = 0
+          if (records.length > 0) {
+            totalAmount = records[0].category.totalAmount
+          } else {
+            totalAmount = 0
+          }
           return res.render('index', { categories, records: slashRecords, totalAmount })
         })
         .catch(error => console.log(error))
