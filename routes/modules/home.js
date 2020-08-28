43aa982e398f7ffac271594ac713d2876d9f4e1e
icedike <icedike@gmail.com>
2020-08-29 01:13:09 +0800

    refactor: modify home route to async/await style

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,55 +3,51 @@ const express = require('express')
 const { Category, Record } = require('../../models/record')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  Category.find()
-    .lean()
-    .then(categories => {
-      let totalAmount = 0
-      categories.forEach(function (category) {
-        totalAmount += category.totalAmount
-      })
-      return Record.find()
-        .populate('category')
-        .lean()
-        .then(records => {
-          const slashRecords = []
-          records.forEach(function (record) {
-            record.date = record.date.replace(/-/g, '/')
-            slashRecords.push(record)
-          })
-          return res.render('index', { categories, records: slashRecords, totalAmount })
-        })
-        .catch(error => console.log(error))
+router.get('/', async (req, res) => {
+  let totalAmount = 0
+  const slashRecords = []
+  let categories = []
+  try {
+    categories = await Category.find().lean()
+    categories.forEach(function (category) {
+      totalAmount += category.totalAmount
     })
-    .catch(error => console.log(error))
+  } catch (error) {
+    console.log(error)
+  }
+  try {
+    const records = await Record.find().lean()
+    records.forEach(function (record) {
+      record.date = record.date.replace(/-/g, '/')
+      slashRecords.push(record)
+      res.render('index', { categories, records: slashRecords, totalAmount })
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
-router.get('/sort', (req, res) => {
+router.get('/sort', async (req, res) => {
   const categoryId = req.query.sort_id
-  Category.find()
-    .lean()
-    .then(categories => {
-      return Record.find({ category: categoryId })
-        .populate('category')
-        .lean()
-        .then(records => {
-          const slashRecords = []
-          records.forEach(function (record) {
-            record.date = record.date.replace(/-/g, '/')
-            slashRecords.push(record)
-          })
-          let totalAmount = 0
-          if (records.length > 0) {
-            totalAmount = records[0].category.totalAmount
-          } else {
-            totalAmount = 0
-          }
-          return res.render('index', { categories, records: slashRecords, totalAmount })
-        })
-        .catch(error => console.log(error))
+  let categories = []
+  try {
+    categories = await Category.find().lean()
+  } catch (error) {
+    console.log(error)
+  }
+
+  try {
+    const records = await Record.find({ category: categoryId }).populate('category').lean()
+    const slashRecords = []
+    records.forEach(function (record) {
+      record.date = record.date.replace(/-/g, '/')
+      slashRecords.push(record)
     })
-    .catch(error => console.log(error))
+    const totalAmount = records.length > 0 ? records[0].category.totalAmount : 0
+    return res.render('index', { categories, records: slashRecords, totalAmount })
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
