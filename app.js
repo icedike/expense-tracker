e9e4a5e7263abce5159d6c43d1101defefdf1afa
icedike <icedike@gmail.com>
2020-09-03 15:34:07 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 require('./config/mongoose')
 
 const app = express()
@@ -22,10 +23,12 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
